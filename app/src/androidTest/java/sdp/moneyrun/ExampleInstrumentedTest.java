1fbb9e2a2c9e487237b6de1f0d1864505848edaa
your_username <alvarocauderan8515@gmail.com>
2021-03-05 19:45:21 +0100

    fixed package name typo

--- app/src/androidTest/java/sdp/moneyrun/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/sdp/moneyrun/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.github.alvaro2112.moneyrun;
+package sdp.moneyrun;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.github.alvaro2112.moneyrun", appContext.getPackageName());
+        assertEquals("sdp.moneyrun", appContext.getPackageName());
     }
 }
