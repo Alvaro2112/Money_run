6bfdf3a9ca9a691a786ecd481b0911794819e80b
gauthierboeshertz <gauthier.boeshertz@gmail.com>
2021-03-09 15:07:14 +0100

    login interface done

--- app/src/main/java/sdp/moneyrun/loginActivity.java
+++ app/src/main/java/sdp/moneyrun/loginActivity.java
@@ -14,8 +14,6 @@ public class loginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
     }
 
     public void signUp(View view) {
