ad4ab4461e904aaeb8b5c966dcdc92a054d1de3e
your_username <alvarocauderan8515@gmail.com>
2021-03-09 22:31:47 +0100

    fix jacoco error

--- app/src/main/java/sdp/moneyrun/MainActivity2.java
+++ app/src/main/java/sdp/moneyrun/MainActivity2.java
@@ -1,11 +1,8 @@
 package sdp.moneyrun;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -14,7 +11,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity2 extends AppCompatActivity {
 
