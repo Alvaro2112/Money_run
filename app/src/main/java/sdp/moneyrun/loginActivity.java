1cb6de9e4381859c1d8a2e49ed908c5cb73189ee
gauthierboeshertz <gauthier.boeshertz@gmail.com>
2021-03-09 15:05:11 +0100

    fix the jacoco bug by removing all wear occurences

--- app/src/main/java/sdp/moneyrun/loginActivity.java
+++ app/src/main/java/sdp/moneyrun/loginActivity.java
@@ -1,20 +1,27 @@
 package sdp.moneyrun;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class loginActivity extends AppCompatActivity {
 
-    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mTextView = (TextView) findViewById(R.id.text);
 
     }
+
+    public void signUp(View view) {
+
+        Intent intent = new Intent(this, placeHolderSignUp.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/sdp/moneyrun/placeHolderSignUp.java
@@ -0,0 +1,14 @@
+package sdp.moneyrun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class placeHolderSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_holder_sign_up);
+    }
+}
