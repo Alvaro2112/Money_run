6f026b09543abbe38fc61e424bdb415033a6a12a
Johann Pluss <johann.epfl.ch>
2021-03-10 19:31:42 +0100

    Modified Gradle files according to Firebase instructions

---
+++ app/src/main/java/sdp/moneyrun/SignUpInterface.java
@@ -0,0 +1,14 @@
+package sdp.moneyrun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_interface);
+    }
+}
