1e871cdb81069a250f7dee745619e167e8c0d715
Rafael <rafael.ramirezserrano@epfl.ch>
2021-03-11 21:48:46 +0100

    implement login, untested

---
+++ app/src/main/java/sdp/moneyrun/Dashboard.java
@@ -0,0 +1,34 @@
+package sdp.moneyrun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Dashboard extends AppCompatActivity {
+    private  Button signOutBtn;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signOutBtn = findViewById(R.id.sign_out_btn);
+        signOutBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
