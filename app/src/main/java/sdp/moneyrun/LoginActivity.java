73073daecab0fee3c1480fbe5eef0ae9b8c91420
gauthierboeshertz <gauthier.boeshertz@gmail.com>
2021-03-11 18:55:42 +0100

    signup button test

--- app/src/main/java/sdp/moneyrun/LoginActivity.java
+++ app/src/main/java/sdp/moneyrun/LoginActivity.java
@@ -7,7 +7,7 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class loginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
 
     @Override
