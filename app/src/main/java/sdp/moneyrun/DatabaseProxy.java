9f045784e3928048c2dba6e8af40fb6537ccb853
Johann Pluss <johann.epfl.ch>
2021-03-12 14:44:32 +0100

    Removed files belonging to other branch

--- app/src/main/java/sdp/moneyrun/DatabaseProxy.java
+++ app/src/main/java/sdp/moneyrun/DatabaseProxy.java
@@ -1,4 +1,39 @@
 package sdp.moneyrun;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DatabaseProxy {
+    private final DatabaseReference mDataBase;
+
+    public DatabaseProxy(){
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public void putPlayer(Player player){
+        mDataBase.child("players").child(String.valueOf(player.getPlayerId())).setValue(player);
+    }
+
+    public String getPlayer(int playerId){
+        return mDataBase.child("players").child(String.valueOf(playerId)).get().addOnCompleteListener(
+                new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if(!task.isSuccessful()){
+                            Log.e("firebase", "Error getting data", task.getException());
+                        }
+                        else{
+                            Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                        }
+                    }
+                }
+        ).getResult().toString();
+    }
 }
--- app/src/test/java/sdp/moneyrun/DatabaseProxyTest.java
+++ app/src/test/java/sdp/moneyrun/DatabaseProxyTest.java
@@ -1,4 +1,16 @@
 package sdp.moneyrun;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 public class DatabaseProxyTest {
+    @Test
+    public void getPlayerFromDatabase(){
+        Player player = new Player(1234);
+        player.setAddress("FooBarr");
+        player.setName("John Doe");
+        DatabaseProxy db = new DatabaseProxy();
+        db.putPlayer(player);
+        System.out.println(db.getPlayer(player.getPlayerId()));
+    }
 }
