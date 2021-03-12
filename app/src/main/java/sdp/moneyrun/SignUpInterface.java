e0c16664f38e98949f9750c9df4244554cc76960
Johann Pluss <johann.epfl.ch>
2021-03-12 15:35:21 +0100

    Added error text view on incorrect submit

--- app/src/main/java/sdp/moneyrun/SignUpInterface.java
+++ app/src/main/java/sdp/moneyrun/SignUpInterface.java
@@ -4,9 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,15 +19,35 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.w3c.dom.Text;
+
 public class SignUpInterface extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    public final String INPUT_ERROR_MESSAGE = "Email and/or password is blank or invalid";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_interface);
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-
+        setErrorMessage();
+        setErrorVisibility(View.INVISIBLE);
+        Button submitButton = (Button) findViewById(R.id.signUpSubmitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View clicked) {
+                String email = SignUpInterface.this.findViewById(R.id.signUpEmailText).toString();
+                String password = SignUpInterface.this.findViewById(R.id.signUpPassword).toString();
+                if (email != null && password != null && SignUpInterface.this.isEmailValid(email)) {
+                    setErrorVisibility(View.INVISIBLE);
+                    SignUpInterface.this.submitSignUp(email, password);
+                } else {
+                    setErrorVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     @Override
@@ -32,15 +56,11 @@ public class SignUpInterface extends AppCompatActivity {
         //Check if user is signed in (non-null) and update UI accordingly
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
-            currentUser.reload();
+            updateUI(currentUser);
         }
     }
 
-
-    protected void submitSignUp(){
-        String email = findViewById(R.id.signUpEmailText).toString();
-        String password = findViewById(R.id.signUpPassword).toString();
-        if (email != null && password != null && isEmailValid(email)){
+    private void submitSignUp(String email, String password){
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(SignUpInterface.this, new OnCompleteListener<AuthResult>(){
                     @Override
@@ -58,17 +78,30 @@ public class SignUpInterface extends AppCompatActivity {
                         }
                     }
                 });
-        }
-
     }
+
+
 
     private void updateUI(FirebaseUser user) {
-        
+        if(user != null){
+            // Intent intent = new Intent (MenuActivity.class);
+            //  startActivity(intent);
+        }
     }
 
 
-    boolean isEmailValid(CharSequence email) {
+    private boolean isEmailValid(CharSequence email) {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private void setErrorMessage(){
+        TextView errorView = (TextView) findViewById(R.id.SignUpErorMessage);
+        errorView.setText(INPUT_ERROR_MESSAGE);
+    }
+
+    private void setErrorVisibility(int visibility){
+        TextView errorView = (TextView) findViewById(R.id.SignUpErorMessage);
+        errorView.setVisibility(visibility);
     }
 
 }
