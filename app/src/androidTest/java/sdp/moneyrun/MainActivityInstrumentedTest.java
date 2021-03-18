cdbde26ac9162ccca765b1e58659d1ce33a7ba77
Rafael <rafael.ramirezserrano@epfl.ch>
2021-03-18 11:33:51 +0100

    improve login UI, InstrumentTest Main Activity

---
+++ app/src/androidTest/java/sdp/moneyrun/MainActivityInstrumentedTest.java
@@ -0,0 +1,36 @@
+package sdp.moneyrun;
+
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+
+    @Test
+    public void activityChangesToDashBoardAfter3Sec(){
+        Intents.init();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        intended(hasComponent(LoginActivity.class.getName()));
+        Intents.release();
+    }
+}
--- app/src/main/java/sdp/moneyrun/MainActivity.java
+++ app/src/main/java/sdp/moneyrun/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_titlescreen);
+        setContentView(R.layout.activity_main);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
