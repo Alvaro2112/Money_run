e3d0845d8e1324ae1b416462a11f616987fbe1d3
Rafael <rafael.ramirezserrano@epfl.ch>
2021-03-22 19:08:33 +0100

    add all buttons to lobbyClass

--- app/src/main/java/sdp/moneyrun/GameLobbyActivity.java
+++ app/src/main/java/sdp/moneyrun/GameLobbyActivity.java
@@ -2,18 +2,34 @@ package sdp.moneyrun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class GameLobbyActivity extends AppCompatActivity {
+    private Button leave;
+    private Button launch;
+    private TextView playersMissing;
+    private TextView playerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_lobby);
 
-        
+        leave = (Button) findViewById(R.id.leave_lobby_button);
+        launch = (Button) findViewById(R.id.launch_game_button);
+        playersMissing = (TextView) findViewById(R.id.players_missing_TextView);
+        playerList = (TextView) findViewById(R.id.player_list_textView);
 
-
+        Intent intent = getIntent();
 
     }
+
+
+
+
 }
