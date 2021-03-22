a4ecdb8f09bf422a63412d376297037f784dfa08
Rafael <rafael.ramirezserrano@epfl.ch>
2021-03-22 17:50:44 +0100

    create gameLobbyActivity

---
+++ app/src/main/java/sdp/moneyrun/GameLobbyActivity.java
@@ -0,0 +1,14 @@
+package sdp.moneyrun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameLobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_lobby);
+    }
+}
