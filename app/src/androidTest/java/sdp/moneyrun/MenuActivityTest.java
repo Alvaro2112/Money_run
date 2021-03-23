d251b75e6b44f1dfc425957d77a64bdc643a7e6d
your_username <alvarocauderan8515@gmail.com>
2021-03-23 17:19:43 +0100

    added tests)

--- app/src/androidTest/java/sdp/moneyrun/MenuActivityTest.java
+++ app/src/androidTest/java/sdp/moneyrun/MenuActivityTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static org.junit.Assert.assertEquals;
@@ -29,10 +30,29 @@ public class MenuActivityTest {
     }
 
     @Test
-    public void popupIsDisplayed() {
+    public void joinGamePopupIsDisplayed() {
         Intents.init();
         onView(ViewMatchers.withId(R.id.join_game)).perform(ViewActions.click());
         onView(ViewMatchers.withId(R.id.popup)).check(matches(isDisplayed()));
+        Intents.release();
+    }
+
+    @Test
+    public void askQuestionPopupIsDisplayed() {
+        Intents.init();
+        onView(ViewMatchers.withId(R.id.ask_question)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.question_popup)).check(matches(isDisplayed()));
+        Intents.release();
+    }
+
+    @Test
+    public void askQuestionPopupClosesWhenFirstAnswerClicked() {
+        Intents.init();
+        onView(ViewMatchers.withId(R.id.ask_question)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.question_choice_1)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.question_popup)).check(doesNotExist());
+
+
         Intents.release();
     }
 
