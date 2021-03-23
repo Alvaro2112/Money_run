bb87a23409a9fc9b34d488135da739ce524d7390
your_username <alvarocauderan8515@gmail.com>
2021-03-23 15:58:08 +0100

    added question popup interface

--- app/src/main/java/sdp/moneyrun/MenuActivity.java
+++ app/src/main/java/sdp/moneyrun/MenuActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -13,10 +14,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.concurrent.TimeUnit;
+
 public class MenuActivity extends AppCompatActivity /*implements NavigationView.OnNavigationItemSelectedListener*/ {
     private Button profileButton;
 
     private Button joinGame;
+    private Button askQuestion;
     private String[] result;
     private Player player;
 
@@ -27,6 +31,7 @@ public class MenuActivity extends AppCompatActivity /*implements NavigationView.
         NavigationView navigationView = findViewById(R.id.nav_view);
         profileButton = findViewById(R.id.go_to_profile_button);
         joinGame = findViewById(R.id.join_game);
+        askQuestion = findViewById(R.id.ask_question);
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,27 +44,66 @@ public class MenuActivity extends AppCompatActivity /*implements NavigationView.
         joinGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onButtonShowPopupWindowClick(v);
+                onButtonShowJoinGamePopupWindowClick(v, true, R.layout.join_game_popup);
+            }
+        });
+
+        askQuestion.setOnClickListener(new View.OnClickListener() {
+            //question = getQuestion();
+            @Override
+            public void onClick(View v) {
+                onButtonShowQuestionPopupWindowClick(v, false, R.layout.question_popup);
             }
         });
     }
 
-    public void onButtonShowPopupWindowClick(View view) {
+    public void onButtonShowJoinGamePopupWindowClick(View view, Boolean focusable, int layoutId) {
 
         // inflate the layout of the popup window
         LayoutInflater inflater = (LayoutInflater)
                 getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.join_game_popup, null);
+        View popupView = inflater.inflate(layoutId, null);
 
         // create the popup window
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
 
         // show the popup window at wanted location
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
 
+    }
 
+    public void onButtonShowQuestionPopupWindowClick(View view, Boolean focusable, int layoutId) {
+
+
+        PopupWindow popupWindow = onButtonShowPopupWindowClick(view, focusable, layoutId);
+
+        popupWindow.getContentView().findViewById(R.id.question_choice_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+
+    }
+
+    public PopupWindow onButtonShowPopupWindowClick(View view, Boolean focusable, int layoutId) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(layoutId, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window at wanted location
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        return popupWindow;
     }
 
 //    @Override
