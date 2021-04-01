f714cb1cea5a8ad33f5c4f2dca486a14595ac647
your_username <alvarocauderan8515@gmail.com>
2021-04-01 16:20:36 +0200

    Revert "Merge pull request #120 from Alvaro2112/feature/addLogoutButton"
    This reverts commit a2323d0777c35798b2fc4c1a9eff6773e74181e3, reversing
    changes made to 26ae11d6a22d9a73952bf4c2c3bccccca56977f3.
    
    Undo bad merge

--- app/src/main/java/sdp/moneyrun/Game.java
+++ app/src/main/java/sdp/moneyrun/Game.java
@@ -3,49 +3,12 @@ package sdp.moneyrun;
 import android.location.Location;
 
 
+
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-class Riddle {
-    private final String question;
-    private final String[] possibleAnswers;
-    private final String correctAnswer;
-
-
-    /**
-     * @param question        This is the question that the user will see and will have to solve
-     * @param possibleAnswers This is all the possible answers the users will be offered
-     * @param correctAnswer   This is the unique correct answer to the question
-     */
-    public Riddle(String question, String[] possibleAnswers, String correctAnswer) {
-        if (question == null || possibleAnswers == null || correctAnswer == null)
-            throw new IllegalArgumentException("Null arguments in Riddle constructor");
-
-        Set<String> possibleAnswersAsSet = new HashSet<>(Arrays.asList(possibleAnswers));
-
-        if (!possibleAnswersAsSet.contains(correctAnswer)) {
-            throw new IllegalArgumentException("The correct solution must be one of the possible answers");
-        }
-
-        this.question = question;
-        this.possibleAnswers = possibleAnswers;
-        this.correctAnswer = correctAnswer;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public String getAnswer() {
-        return correctAnswer;
-    }
-
-    public String[] getPossibleAnswers() {
-        return possibleAnswers;
-    }
-}
 
 
 // The entirety of the game logic should be implemented in this class
--- app/src/main/java/sdp/moneyrun/MenuActivity.java
+++ app/src/main/java/sdp/moneyrun/MenuActivity.java
@@ -2,7 +2,6 @@ package sdp.moneyrun;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +13,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.InputStream;
 
 public class MenuActivity extends AppCompatActivity /*implements NavigationView.OnNavigationItemSelectedListener*/ {
 
@@ -23,38 +23,35 @@ public class MenuActivity extends AppCompatActivity /*implements NavigationView.
     private Button joinGame;
     private String[] result;
     private Player player;
+    private RiddlesDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-      
+
+        try{
+            db = RiddlesDatabase.createInstance(getApplicationContext());
+        }
+        catch(RuntimeException e){
+            db = RiddlesDatabase.getInstance();
+        }
+
         NavigationView navigationView = findViewById(R.id.nav_view);
         profileButton = findViewById(R.id.go_to_profile_button);
         leaderboardButton = findViewById(R.id.menu_leaderboardButton);
-      
+
         addJoinGameButtonFunctionality();
         addAskQuestionButtonFunctionality();
-        addLogOutButtonFunctionality();
         linkProfileButton(profileButton);
         linkLeaderboardButton(leaderboardButton);
     }
 
-    public void addLogOutButtonFunctionality(){
-
-        Button logOut = findViewById(R.id.log_out_button);
-
-        /**
-         * Checks for clicks on the join game button and creates a popup of available games if clicked
-         */
-        logOut.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                finish();
-            }
-        });
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        RiddlesDatabase.reset();
     }
 
     public void addJoinGameButtonFunctionality(){
@@ -83,12 +80,8 @@ public class MenuActivity extends AppCompatActivity /*implements NavigationView.
         askQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Temporary, will be removed when questions are added to the database
-                String question = "One of these four countries does not border the Red Sea.";
-                String correctAnswer = "Oman";
-                String[] possibleAnswers = {"Jordan", "Oman", "Sudan"};
-                Riddle riddle = new Riddle(question, possibleAnswers, correctAnswer);
-                onButtonShowQuestionPopupWindowClick(v, true, R.layout.question_popup, riddle);
+
+                onButtonShowQuestionPopupWindowClick(v, true, R.layout.question_popup, db.getRandomRiddle());
             }
         });
 
@@ -147,12 +140,10 @@ public class MenuActivity extends AppCompatActivity /*implements NavigationView.
 
         //Loops to find the ID of the button solution and assigns the text to each button
         for (int i = 0; i < 4; i++){
-            if(i >= riddle.getPossibleAnswers().length){
-                popupWindow.getContentView().findViewById(buttonIds[i]).setVisibility(View.GONE);
-                continue;
-            }
+
             buttonView = popupWindow.getContentView().findViewById(buttonIds[i]);
             buttonView.setText(riddle.getPossibleAnswers()[i]);
+
             if(riddle.getPossibleAnswers()[i].equals(riddle.getAnswer()))
                 correctId = buttonIds[i];
         }
