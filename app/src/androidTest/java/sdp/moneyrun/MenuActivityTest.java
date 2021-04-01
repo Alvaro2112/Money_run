7876919b13d6da29546ea3f8d0422a4e262bbd50
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 20:53:43 +0200

    Added instrumented tests for new game.

--- app/src/androidTest/java/sdp/moneyrun/MenuActivityTest.java
+++ app/src/androidTest/java/sdp/moneyrun/MenuActivityTest.java
@@ -1,6 +1,9 @@
 package sdp.moneyrun;
 
 import android.content.Context;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.lifecycle.Lifecycle.State;
 import androidx.test.core.app.ActivityScenario;
@@ -12,11 +15,16 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
@@ -31,6 +39,26 @@ public class MenuActivityTest {
     @Rule
     public ActivityScenarioRule<MenuActivity> testRule = new ActivityScenarioRule<>(MenuActivity.class);
 
+    //adapted from https://stackoverflow.com/questions/28408114/how-can-to-test-by-espresso-android-widget-textview-seterror/28412476
+    private static Matcher<View> withError(final String expected) {
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof EditText)) {
+                    return false;
+                }
+                EditText editText = (EditText) view;
+                return editText.getError().toString().equals(expected);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+
+            }
+        };
+    }
+
     @Test
     public void activityStartsProperly() {
         assertEquals(State.RESUMED, testRule.getScenario().getState());
@@ -40,6 +68,12 @@ public class MenuActivityTest {
     public void joinGamePopupIsDisplayed() {
         onView(ViewMatchers.withId(R.id.join_game)).perform(ViewActions.click());
         onView(ViewMatchers.withId(R.id.join_popup)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void newGamePopupIsDisplayed() {
+        onView(ViewMatchers.withId(R.id.new_game)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.new_game_popup)).check(matches(isDisplayed()));
     }
 
     @Test
@@ -64,6 +98,65 @@ public class MenuActivityTest {
         Espresso.onView(withId(R.id.log_out_button)).perform(ViewActions.click());
         Thread.sleep(1000);
         assertEquals(State.DESTROYED, testRule.getScenario().getState());
+    }
+
+    @Test
+    public void newGameEmptyNameFieldError() {
+        try (ActivityScenario<MenuActivity> scenario = ActivityScenario.launch(MenuActivity.class)) {
+            Intents.init();
+            onView(ViewMatchers.withId(R.id.new_game)).perform(ViewActions.click());
+
+            final String game_name = "new game";
+            final String max_player_count = "12";
+            final String expected = "This field is required";
+
+            Espresso.onView(withId(R.id.newGameSubmit)).perform(ViewActions.click());
+            Espresso.onView(withId(R.id.nameGameText)).check(matches(withError(expected)));
+
+            Espresso.onView(withId(R.id.maxPlayerNumber)).perform(typeText(max_player_count), closeSoftKeyboard());
+            Espresso.onView(withId(R.id.newGameSubmit)).perform(ViewActions.click());
+            Espresso.onView(withId(R.id.nameGameText)).check(matches(withError(expected)));
+
+            Intents.release();
+        }
+    }
+
+    @Test
+    public void newGameEmptyMaxPlayerCountFieldError() {
+        try (ActivityScenario<MenuActivity> scenario = ActivityScenario.launch(MenuActivity.class)) {
+            Intents.init();
+            onView(ViewMatchers.withId(R.id.new_game)).perform(ViewActions.click());
+
+            final String game_name = "new game";
+            final String max_player_count_zero = "0";
+            final String expected = "This field is required";
+            final String expected_zero_players = "There should be at least one player in a game";
+
+            Espresso.onView(withId(R.id.nameGameText)).perform(typeText(game_name), closeSoftKeyboard());
+            Espresso.onView(withId(R.id.newGameSubmit)).perform(ViewActions.click());
+            Espresso.onView(withId(R.id.maxPlayerNumber)).check(matches(withError(expected)));
+
+            Intents.release();
+        }
+    }
+
+    @Test
+    public void newGameZeroMaxPlayerCountFieldError() {
+        try (ActivityScenario<MenuActivity> scenario = ActivityScenario.launch(MenuActivity.class)) {
+            Intents.init();
+            onView(ViewMatchers.withId(R.id.new_game)).perform(ViewActions.click());
+
+            final String game_name = "new game";
+            final String max_player_count_zero = "0";
+            final String expected_zero_players = "There should be at least one player in a game";
+
+            Espresso.onView(withId(R.id.nameGameText)).perform(typeText(game_name), closeSoftKeyboard());
+            Espresso.onView(withId(R.id.maxPlayerNumber)).perform(typeText(max_player_count_zero), closeSoftKeyboard());
+            Espresso.onView(withId(R.id.newGameSubmit)).perform(ViewActions.click());
+            Espresso.onView(withId(R.id.maxPlayerNumber)).check(matches(withError(expected_zero_players)));
+
+            Intents.release();
+        }
     }
 
 
