6bcbab870d068c5302b9c75b23b8317173909256
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 13:19:47 +0200

    Updated game class

--- app/src/main/java/sdp/moneyrun/Game.java
+++ app/src/main/java/sdp/moneyrun/Game.java
@@ -13,29 +13,50 @@ import java.util.Set;
 
 // The entirety of the game logic should be implemented in this class
 public class Game {
+    private final String gameId;
+    private final String name;
     private List<Player> players;
+    private int playerCount;
+    private int maxPlayerCount;
     private List<Riddle> riddles;
-    private Location startLocation;//TODO: check if we will use the existing or create a new class Location
+    private Location startLocation; //TODO: check if we will use the existing or create a new class Location
 
-    public Game(List<Player> players, List<Riddle> riddles, Location startLocation) {
-        if (players == null || riddles == null || startLocation == null || players.isEmpty() || riddles.isEmpty())
+    public Game(String gameId, String name, List<Player> players, int maxPlayerCount, List<Riddle> riddles, Location startLocation){
+        if(name == null || players == null || riddles == null || startLocation == null)
             throw new IllegalArgumentException("Null parameter passed as argument in Game constructor");
-
+        this.gameId = gameId;
+        this.name = name;
         this.players = players;
+        this.playerCount = 0;
+        this.maxPlayerCount = maxPlayerCount;
         this.riddles = riddles;
         this.startLocation = startLocation;
     }
 
-    public static void startGame(Game game) {
-        game.startGame();
+    public String getGameId(){
+        return gameId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPlayerCount(){
+        return players.size();
+    }
+
+    public int getMaxPlayerCount(){
+        return maxPlayerCount;
     }
 
     // Launched when create game button is pressed
-    public void startGame() {
+    public void startGame(){
 
     }
-
-    public boolean askPlayer(Player player, Riddle riddle) {
+    public static void startGame(Game game){
+        game.startGame();
+    }
+    public boolean askPlayer(Player player, Riddle riddle){
         String playerResponse = player.ask(riddle.getQuestion());
         return playerResponse.trim().replaceAll(" ", "").toLowerCase().equals(riddle.getAnswer());
     }
