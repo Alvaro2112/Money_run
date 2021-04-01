1a5dc85b22de51340f0bfa45ffc6652a336e2026
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 20:24:35 +0200

    modified menu activity, merge

--- app/src/main/java/sdp/moneyrun/MenuActivity.java
+++ app/src/main/java/sdp/moneyrun/MenuActivity.java
@@ -2,6 +2,7 @@ package sdp.moneyrun;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,12 +17,15 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
