be7390ddd94f6f56f226fb2240a9e830d93b2de3
BuildTools <unconfigured@null.spigotmc.org>
2021-04-02 10:59:29 +0200

    added missing tests

--- app/src/androidTest/java/sdp/moneyrun/GameLobbyActivityInstrumentedTest.java
+++ app/src/androidTest/java/sdp/moneyrun/GameLobbyActivityInstrumentedTest.java
@@ -1,6 +1,9 @@
 package sdp.moneyrun;
 
 import androidx.lifecycle.Lifecycle;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -8,6 +11,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
@@ -19,5 +25,19 @@ public class GameLobbyActivityInstrumentedTest {
     @Test
     public void activityStartsProperly() {
         assertEquals(Lifecycle.State.RESUMED, testRule.getScenario().getState());
+    }
+
+    @Test
+    public void LeaveLobbyWorks() throws InterruptedException {
+        try {
+            Intents.init();
+            onView(ViewMatchers.withId(R.id.leave_lobby_button)).perform(ViewActions.click());
+            Thread.sleep(1000);
+            intended(hasComponent(MenuActivity.class.getName()));
+            Intents.release();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+            Intents.release();
+        }
     }
 }
