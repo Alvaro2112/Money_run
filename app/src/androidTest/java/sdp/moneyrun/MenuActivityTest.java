7586dfeda9f831aa326485dfdcd2eeeca1e4db54
BuildTools <unconfigured@null.spigotmc.org>
2021-04-09 17:19:33 +0200

    edited tests

--- app/src/androidTest/java/sdp/moneyrun/MenuActivityTest.java
+++ app/src/androidTest/java/sdp/moneyrun/MenuActivityTest.java
@@ -177,6 +177,13 @@ public class MenuActivityTest {
             final String expected_zero_players = "There should be at least one player in a game";
 
             Espresso.onView(withId(R.id.nameGameText)).perform(typeText(game_name), closeSoftKeyboard());
+
+            try {
+                Thread.sleep(100);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
             Espresso.onView(withId(R.id.maxPlayerNumber)).perform(typeText(max_player_count_zero), closeSoftKeyboard());
             Espresso.onView(withId(R.id.newGameSubmit)).perform(ViewActions.click());
 
