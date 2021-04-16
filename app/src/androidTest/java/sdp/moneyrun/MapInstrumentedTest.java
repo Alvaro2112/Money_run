bcf77ce2ccb6e38d24f2cc86d50a3d36ad6e3125
your_username <alvarocauderan8515@gmail.com>
2021-04-16 15:18:28 +0200

    added tests and modified map layout to be able to add constraints

--- app/src/androidTest/java/sdp/moneyrun/MapInstrumentedTest.java
+++ app/src/androidTest/java/sdp/moneyrun/MapInstrumentedTest.java
@@ -3,7 +3,9 @@ package sdp.moneyrun;
 import android.content.Context;
 import android.location.Location;
 
+import androidx.lifecycle.Lifecycle;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -312,6 +314,28 @@ public class MapInstrumentedTest {
             Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
             intended(hasComponent(EndGameActivity.class.getName()));
+            Intents.release();
+        }
+    }
+
+    @Test
+    public void questionButtonWorks(){
+
+        try (ActivityScenario<MapActivity> scenario = ActivityScenario.launch(MapActivity.class)) {
+            onView(ViewMatchers.withId(R.id.new_question)).perform(ViewActions.click());
+            onView(ViewMatchers.withId(R.id.ask_question_popup)).check(matches(isDisplayed()));
+        }
+    }
+
+    @Test
+    public void closeButtonWorks(){
+
+        try (ActivityScenario<MapActivity> scenario = ActivityScenario.launch(MapActivity.class)) {
+            Intents.init();
+
+            onView(ViewMatchers.withId(R.id.close_map)).perform(ViewActions.click());
+            assertEquals(Lifecycle.State.DESTROYED, scenario.getState());
+
             Intents.release();
         }
     }
