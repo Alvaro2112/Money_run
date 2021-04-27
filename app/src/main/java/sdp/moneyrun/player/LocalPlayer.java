2f65db436dc4a93d7543e00a0dd177b4b169e859
your_username <alvarocauderan8515@gmail.com>
2021-04-27 13:36:48 +0200

    added throws for incorrect arguments to LocalPlayer class

--- app/src/main/java/sdp/moneyrun/player/LocalPlayer.java
+++ app/src/main/java/sdp/moneyrun/player/LocalPlayer.java
@@ -10,17 +10,53 @@ import sdp.moneyrun.map.Coin;
  */
 public class LocalPlayer  implements Serializable {
 
-    private ArrayList<Coin> lostCoins;
 
-    public void LocalPlayer(){
+    private ArrayList<Coin> lostCoins;
+    private ArrayList<Coin> locallyAvailableCoins;
+
+    public ArrayList<Coin> getLocallyAvailableCoins() {
+        return locallyAvailableCoins;
+    }
+
+    public ArrayList<Coin> getLostCoins() {
+        return lostCoins;
+    }
+
+    public LocalPlayer(){
      this.lostCoins = new ArrayList<Coin>();
     }
 
     public void addLostCoin(Coin coin){
+        if(coin == null){
+            throw new IllegalArgumentException("The coin to be added cannot be null");
+        }
+
         lostCoins.add(coin);
     }
 
+    public void updateLostCoins(ArrayList<Coin> availableCoins){
+        if(availableCoins == null){
+            throw new IllegalArgumentException("The availableCoins cannot be null");
+        }
+        if(availableCoins.contains(null)){
+            throw new IllegalArgumentException("The availableCoins cannot contain a null coin");
+        }
 
+        ArrayList<Coin> toRemove = lostCoins;
+        lostCoins.removeAll(availableCoins);
+        this.lostCoins.removeAll(toRemove);
+    }
 
+    public void computeLocallyAvailableCoins(ArrayList<Coin> availableCoins){
+        if(availableCoins == null){
+            throw new IllegalArgumentException("The availableCoins cannot be null");
+        }
+        if(availableCoins.contains(null)){
+            throw new IllegalArgumentException("The availableCoins cannot contain a null coin");
+        }
+
+        this.locallyAvailableCoins = availableCoins;
+        this.locallyAvailableCoins.removeAll(lostCoins);
+    }
 
 }
