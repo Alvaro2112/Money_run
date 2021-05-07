403a4ce630ff335c0e65f20f8d5fecc56eba9157
ArnaudPoletto <arnaud.poletto@epfl.ch>
2021-05-07 17:53:04 +0200

    edit code to for cognitive complexity

--- app/src/main/java/sdp/moneyrun/database/GameDatabaseProxy.java
+++ app/src/main/java/sdp/moneyrun/database/GameDatabaseProxy.java
@@ -54,12 +54,9 @@ public class GameDatabaseProxy extends DatabaseProxy {
         }
 
         if(!game.getHasBeenAdded()) {
-            String id = game.getId();
+            String id = game.getId() != null ? game.getId() : gamesRef.push().getKey();
             if(id == null){
-                id = gamesRef.push().getKey();
-                if(id == null){
-                    throw new IllegalArgumentException("Could not add game to database, id is null.");
-                }
+                throw new IllegalArgumentException("Could not add game to database, id is null.");
             }
             game.setId(id);
             gamesRef.child(id).setValue(game.getGameDbData());
