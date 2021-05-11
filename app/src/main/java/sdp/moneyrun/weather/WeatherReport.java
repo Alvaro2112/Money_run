9ecb50644f9d39e0f810daa4419abf030669ec9d
your_username <alvarocauderan8515@gmail.com>
2021-05-11 19:13:54 +0200

    added the WeatherReport class

---
+++ app/src/main/java/sdp/moneyrun/weather/WeatherReport.java
@@ -0,0 +1,58 @@
+package sdp.moneyrun.weather;
+
+public final class WeatherReport {
+    private double averageTemperature;
+    private double minimumTemperature;
+    private double maximalTemperature;
+
+    private String weatherType;
+    private String weatherIcon;
+
+    public WeatherReport(double averageTemperature, double minimumTemperature, double maximalTemperature, String weatherType, String weatherIcon) {
+        this.averageTemperature = averageTemperature;
+        this.minimumTemperature = minimumTemperature;
+        this.maximalTemperature = maximalTemperature;
+        this.weatherType = weatherType;
+        this.weatherIcon = weatherIcon;
+    }
+
+    public double getAverageTemperature() {
+        return averageTemperature;
+    }
+
+    public void setAverageTemperature(double averageTemperature) {
+        this.averageTemperature = averageTemperature;
+    }
+
+    public double getMinimumTemperature() {
+        return minimumTemperature;
+    }
+
+    public void setMinimumTemperature(double minimumTemperature) {
+        this.minimumTemperature = minimumTemperature;
+    }
+
+    public double getMaximalTemperature() {
+        return maximalTemperature;
+    }
+
+    public void setMaximalTemperature(double maximalTemperature) {
+        this.maximalTemperature = maximalTemperature;
+    }
+
+    public String getWeatherType() {
+        return weatherType;
+    }
+
+    public void setWeatherType(String weatherType) {
+        this.weatherType = weatherType;
+    }
+
+    public String getWeatherIcon() {
+        return weatherIcon;
+    }
+
+    public void setWeatherIcon(String weatherIcon) {
+        this.weatherIcon = weatherIcon;
+    }
+}
