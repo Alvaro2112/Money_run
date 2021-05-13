b0a1f6889fa797f3d1289770214c62884d859e0c
ArnaudPoletto <arnaud.poletto@epfl.ch>
2021-05-13 19:22:22 +0200

    added init release

--- app/src/main/java/sdp/moneyrun/menu/MenuImplementation.java
+++ app/src/main/java/sdp/moneyrun/menu/MenuImplementation.java
@@ -79,7 +79,7 @@ public class MenuImplementation {
         // inflate the layout of the popup window
         LayoutInflater inflater = (LayoutInflater)
                 activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(layoutId, null);
+        View popupView = inflater.inflate(layoutId, null, false);
 
         // create the popup window
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
