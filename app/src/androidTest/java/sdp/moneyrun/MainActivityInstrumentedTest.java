0f766009b5eb70ed01684956abfd2e2fd5fb8bad
Rafael <rafael.ramirezserrano@epfl.ch>
2021-05-20 18:52:41 +0200

    remove deprecated test

--- app/src/androidTest/java/sdp/moneyrun/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/sdp/moneyrun/MainActivityInstrumentedTest.java
@@ -22,11 +22,7 @@ public class MainActivityInstrumentedTest {
     @NonNull
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
-
-    @Test
-    public void splashScreenSoundPlays(){
-        
-    }
+    
 
     @Test
     public void activityChangesToDashBoardAfter3Sec() {
