db52cc3c7d52a2a82b2063062aa8f5fec66d84f3
ArnaudPoletto <arnaud.poletto@epfl.ch>
2021-05-22 16:23:30 +0200

    added tests

---
+++ app/src/androidTest/java/sdp/moneyrun/AndroidLocationServiceInstrumentedTest.java
@@ -0,0 +1,77 @@
+package sdp.moneyrun;
+
+import android.content.Intent;
+import android.location.Criteria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import sdp.moneyrun.location.AndroidLocationService;
+import sdp.moneyrun.ui.menu.MenuActivity;
+import sdp.moneyrun.user.User;
+import sdp.moneyrun.location.LocationRepresentation;
+
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.core.app.ApplicationProvider;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(AndroidJUnit4.class)
+public class AndroidLocationServiceInstrumentedTest {
+
+    private Intent getStartIntent() {
+        User currentUser = new User("999", "CURRENT_USER", "Epfl"
+                , 0, 0, 0);
+        Intent toStart = new Intent(ApplicationProvider.getApplicationContext(), MenuActivity.class);
+        toStart.putExtra("user", currentUser);
+        return toStart;
+    }
+
+    @Test
+    public void LocationServiceFromCriteriaIsCorrect() {
+        try (ActivityScenario<MenuActivity> scenario = ActivityScenario.launch(getStartIntent())) {
+            scenario.onActivity(a -> {
+                Criteria criteria = new Criteria();
+                criteria.setAccuracy(Criteria.ACCURACY_FINE);
+                AndroidLocationService locationService = AndroidLocationService.buildFromContextAndCriteria(a.getApplicationContext(), criteria);
+
+                assertEquals(locationService.getLocationCriteria(), criteria);
+            });
+        }
+    }
+
+    @Test
+    public void LocationServiceFromProviderIsCorrect() {
+        try (ActivityScenario<MenuActivity> scenario = ActivityScenario.launch(getStartIntent())) {
+            scenario.onActivity(a -> {
+                Criteria criteria = new Criteria();
+                criteria.setAccuracy(Criteria.ACCURACY_FINE);
+                AndroidLocationService locationService = AndroidLocationService.buildFromContextAndProvider(a.getApplicationContext(), "");
+
+                assertEquals(locationService.getLocationProvider(), "");
+            });
+        }
+    }
+
+    @Test
+    public void getMockedCurrentLocationWorks() {
+        try (ActivityScenario<MenuActivity> scenario = ActivityScenario.launch(getStartIntent())) {
+            scenario.onActivity(a -> {
+                Criteria criteria = new Criteria();
+                criteria.setAccuracy(Criteria.ACCURACY_FINE);
+
+                AndroidLocationService locationService = AndroidLocationService.buildFromContextAndCriteria(a.getApplicationContext(), criteria);
+
+                LocationRepresentation locationRepresentation = new LocationRepresentation(10., 20.);
+                locationService.setMockedLocation(locationRepresentation);
+
+                assertTrue(locationService.isLocationMocked());
+                assertEquals(locationService.getCurrentLocation(), locationRepresentation);
+            });
+        }
+    }
+}
--- app/src/main/java/sdp/moneyrun/location/AndroidLocationService.java
+++ app/src/main/java/sdp/moneyrun/location/AndroidLocationService.java
@@ -1,7 +1,5 @@
 package sdp.moneyrun.location;
 
-import androidx.core.location.LocationManagerCompat;
-
 import android.content.Context;
 import android.location.LocationManager;
 import android.location.Criteria;
@@ -12,13 +10,14 @@ public final class AndroidLocationService implements LocationService {
     private final String locationProvider;
     private final Criteria locationCriteria;
 
-    AndroidLocationService(LocationManager locationManager, String locationProvider, Criteria locationCriteria){
-        if(locationManager == null){
-            throw new IllegalArgumentException("location manager should not be null.");
-        }
+    private boolean isLocationMocked;
+    private LocationRepresentation mockedLocation = null;
+
+    private AndroidLocationService(LocationManager locationManager, String locationProvider, Criteria locationCriteria){
         this.locationManager = locationManager;
         this.locationProvider = locationProvider;
         this.locationCriteria = locationCriteria;
+        this.isLocationMocked = false;
     }
 
     /**
@@ -34,7 +33,7 @@ public final class AndroidLocationService implements LocationService {
      * @param locationProvider the location provider
      * @return a new lcation service with a fixed location provider
      */
-    public static LocationService buildFromContextAndProvider(Context context, String locationProvider) {
+    public static AndroidLocationService buildFromContextAndProvider(Context context, String locationProvider) {
         return new AndroidLocationService(
                 buildLocationManagerFromContext(context),
                 locationProvider,
@@ -47,7 +46,7 @@ public final class AndroidLocationService implements LocationService {
      * @param locationCriteria the location criteria to choose which location provider to use
      * @return a new location service  with the best location provider
      */
-    public static LocationService buildFromContextAndCriteria(Context context, Criteria locationCriteria) {
+    public static AndroidLocationService buildFromContextAndCriteria(Context context, Criteria locationCriteria) {
         return new AndroidLocationService(
                 buildLocationManagerFromContext(context),
                 null,
@@ -55,7 +54,7 @@ public final class AndroidLocationService implements LocationService {
         );
     }
 
-    private String getLocationProvider() {
+    public String getLocationProvider() {
         if (this.locationProvider != null) {
             return this.locationProvider;
         }
@@ -63,10 +62,21 @@ public final class AndroidLocationService implements LocationService {
         return this.locationManager.getBestProvider(this.locationCriteria, true);
     }
 
+    public Criteria getLocationCriteria() {
+        return this.locationCriteria;
+    }
+
+    public boolean isLocationMocked(){
+        return this.isLocationMocked;
+    }
+
     @Override
     public LocationRepresentation getCurrentLocation() {
         try{
-            android.location.Location location = this.locationManager.getLastKnownLocation(this.getLocationProvider());
+            android.location.Location location = this.locationManager.getLastKnownLocation(getLocationProvider());
+            if(this.isLocationMocked){
+                return this.mockedLocation;
+            }
 
             if(location == null){
                 return null;
@@ -76,5 +86,17 @@ public final class AndroidLocationService implements LocationService {
         }catch (SecurityException e){
             throw e;
         }
+    }
+
+    public void setMockedLocation(LocationRepresentation locationRepresentation){
+        if(locationRepresentation == null){
+            throw new IllegalArgumentException("mocked location representation should not be null.");
+        }
+        this.isLocationMocked = true;
+        this.mockedLocation = locationRepresentation;
+    }
+
+    public void resetMockedLocation(){
+        this.isLocationMocked = false;
     }
 }
