e47c40cae5ccb3fb5e9c910c7ad57fb31b1b4bac
gauthierboeshertz <gauthier.boeshertz@gmail.com>
2021-05-27 15:33:25 +0200

    add permission

--- app/src/main/java/sdp/moneyrun/location/MapLocationManager.java
+++ app/src/main/java/sdp/moneyrun/location/MapLocationManager.java
@@ -5,24 +5,33 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 
 
 public class MapLocationManager  {
 
-    private Location mockedLocation;
     private LocationManager locationManager;
 
     public MapLocationManager(LocationManager locationManager, String provider){
-        if(provider == null ||provider.equals("test") ){
-            Location mockedLocation = new Location("");//provider name is unnecessary
+        if(locationManager == null){
+            throw new NullPointerException("location manager is null");
+        }
+        System.out.println(provider);
+        if(provider == null ||provider.equals("test_provider") ){
+            System.out.println(provider);
+            locationManager.addTestProvider(provider,false,false,false,false,false,false,false, Criteria.POWER_LOW, Criteria.ACCURACY_FINE);
+            locationManager.setTestProviderStatus(provider, LocationProvider.AVAILABLE,null,System.currentTimeMillis());
+            Location mockedLocation = new Location("");
             mockedLocation.setLatitude(37.42);
             mockedLocation.setLongitude(-122.084);
-            locationManager.addTestProvider("test_provider",false,false,false,false,false,false,false, Criteria.POWER_LOW, Criteria.ACCURACY_FINE);
-            locationManager.setTestProviderLocation("test_provider",mockedLocation);
+
+            locationManager.setTestProviderLocation(provider,mockedLocation);
         }
+        this.locationManager = locationManager;
     }
     @SuppressLint("MissingPermission")
     public void requestLocationUpdates(String provider, long minTimeMs, float minDistanceM, LocationListener listener ){
         locationManager.requestLocationUpdates(provider,minTimeMs,minDistanceM,listener);
+
     }
 }
