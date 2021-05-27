c9fb4219c3271d9f0208a37c7cce9ce86fc00ed1
gauthierboeshertz <gauthier.boeshertz@gmail.com>
2021-05-27 19:55:53 +0200

    remove adofflinemap function

--- app/src/main/java/sdp/moneyrun/ui/menu/MenuActivity.java
+++ app/src/main/java/sdp/moneyrun/ui/menu/MenuActivity.java
@@ -114,7 +114,6 @@ public class MenuActivity extends AppCompatActivity implements NavigationView.On
 
         runFunctionalities();
         addDownloadButton();
-        addOfflineMapButton();
 
         DatabaseProxy.addOfflineListener(MenuActivity.this, TAG);
     }
